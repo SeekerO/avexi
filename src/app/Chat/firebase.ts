9ebd13dc5c2e5807b0c7e86e4bfc01e73a5d3af8
SeekerDev <albertbaisa@gmail.com>
2025-07-21 12:56:22 +0800

    test

--- src/app/Chat/firebase.ts
+++ src/app/Chat/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY_CHAT,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN_CHAT,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL_CHAT,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_CHAT,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_CHAT,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID_CHAT,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID_CHAT,
 };
 
 const app = initializeApp(firebaseConfig);
