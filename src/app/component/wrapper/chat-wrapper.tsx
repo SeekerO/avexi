a0fd9bd4da622c9c3d6fa96bdaf92239f8b4e4bd
SeekerDev <albertbaisa@gmail.com>
2025-10-20 17:32:00 +0800

    [AUTH]

--- src/app/component/wrapper/chat-wrapper.tsx
+++ src/app/component/wrapper/chat-wrapper.tsx
@@ -1,19 +1,23 @@
 // components/Wrapper.tsx
 "use client"
 import PopupChat from "@/app/Chat/PopupChat";
-
+import { useAuth } from "@/app/Chat/AuthContext";
 interface ChatWrapper {
     children: React.ReactNode;
 }
 
 const ChatWrapper: React.FC<ChatWrapper> = ({ children }) => {
-
+    const { user } = useAuth();
     return (
         <>
             {children}
-            <div className='absolute bottom-5 right-5 z-50'>
-                <PopupChat />
-            </div>
+            <>
+                {user && user.canChat !== false &&
+                    <div className='absolute bottom-5 right-5 z-50'>
+                        <PopupChat />
+                    </div>
+                }
+            </>
         </>
     );
 };
